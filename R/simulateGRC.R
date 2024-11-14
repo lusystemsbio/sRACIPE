728dfa7d9f2b5c1b99172ffd38643e8d842e7f33
76aidatill <aidan.tillman18@gmail.com>
2024-11-14 16:23:45 -0500

    Fixed bug with knockout convergence data saving

--- R/simulateGRC.R
+++ R/simulateGRC.R
@@ -1045,8 +1045,8 @@ if(missing(nNoise)){
         #Collects convergence data for every knockout simulation
         if(convergTesting){
           converge <- rbind(converge, do.call(rbind, knockOutDataConv))
-          parameters <- do.call(rbind, replicate(length(knockOut), parameters, simplify = FALSE))
-          ic <- do.call(rbind, replicate(length(knockOut), ic, simplify = FALSE))
+          parameters <- do.call(rbind, replicate(length(knockOut)+1, parameters, simplify = FALSE))
+          ic <- do.call(rbind, replicate(length(knockOut)+1, ic, simplify = FALSE))
         }
         knockOutData <- lapply(knockOutData, t)
         assayDataTmp <- c(assayDataTmp,knockOutData)
